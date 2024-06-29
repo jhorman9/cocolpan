e1ceaf10e8166d57d0939ba535cfaab13e670cb0
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2024-06-29 12:49:10 -0500

    Agregando primer backup

---
+++ js/getImg.js
@@ -0,0 +1,14 @@
+$(window).on("load", function() {
+    "use strict";
+
+
+
+    //  Contact Form Working Functionality
+
+    const getImg = document.querySelectorAll(".img-big");
+
+    getImg.forEach(element => {
+        console.log(element.src)
+    });
+
+}); 
